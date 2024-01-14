c0c7812698f43416b4f0d7f71fad7694c850c6d7
P4trick-Wu <patrickwu.2004@gmail.com>
2024-01-14 12:37:40 -0500

    minor big fixes regarding currentTime variable

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from functions import *
 import datetime
 
 
-
 # https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.109/win32/chromedriver-win32.zip
 # Selenium chrome driver
 userTime = getUserTime()
 
-#converts string userTime to time object
+# converts string userTime to time object
 targetTime = datetime.datetime.strptime(userTime, "%H:%M").time()
 
 currentTime = datetime.datetime.now().time()
 
-while now < targetTime:
-        time.sleep(30)  # Check every 30 seconds
-        now = datetime.datetime.now().time()
+while currentTime < targetTime:
+    time.sleep(30)  # Check every 30 seconds
+    # now = datetime.datetime.now().time()
+    currentTime = datetime.datetime.now().time()
 
 # Emails used for booking
 emails = ["jasonwang9", "durvishanthananchaya", "patrickwu4", "huzaifarehan"]
