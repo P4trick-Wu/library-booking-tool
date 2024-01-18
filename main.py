648ea624ae6e0284043f5c9b68c3d55acc58d1db
P4trick-Wu <patrickwu.2004@gmail.com>
2024-01-17 21:05:04 -0500

    changed implementation of time feature, capable of running at the beginning of the next day I can sleep early now yayyy

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from functions import *
-import datetime
-
+from datetime import datetime
 
 # https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.109/win32/chromedriver-win32.zip
 # Selenium chrome driver
 userTime = getUserTime()
 
-# converts string userTime to time object
-targetTime = datetime.datetime.strptime(userTime, "%H:%M").time()
+# Gets current time, converts to string of Hours:Minutes format
+currentTime = datetime.now().time()
+currentTime = currentTime.strftime("%H:%M")
 
-currentTime = datetime.datetime.now().time()
+while currentTime != userTime:
+    # Check if current time is equal to time set by user every 30 sec
+    time.sleep(30)
 
-while currentTime < targetTime:
-    time.sleep(30)  # Check every 30 seconds
-    # now = datetime.datetime.now().time()
-    currentTime = datetime.datetime.now().time()
+    # Converts current time to string of Hours:Minutes format
+    currentTime = datetime.now().time()
+    currentTime = currentTime.strftime("%H:%M")
 
 # Emails used for booking
 emails = ["jasonwang9", "durvishanthananchaya", "patrickwu4", "huzaifarehan"]
-bookAllRooms(emails)
+# bookAllRooms(emails)
+print("Running script")
